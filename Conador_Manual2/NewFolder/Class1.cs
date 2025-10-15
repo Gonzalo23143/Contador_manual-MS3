56bb86a78316673df3e0a0f9a21b7128d9c8e6b4
Gonzalo Cutz Gasca <24470027@campeche.tecnm.mx|git config user.email 24470027@campeche.tecnm.mx>
2025-10-14 18:20:03 -0600

    Creacion de otra clase

---
+++ Conador_Manual2/NewFolder/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conador_Manual2.NewFolder
+{
+    class Class1
+    {
+        private int _conteo;
+
+        public int Conteo
+        {
+            get => _conteo;
+            set
+            {
+                if (_conteo != value)
+                {
+                    _conteo = value;
+                }
+            }
+    }
+
+        public  Class1()
+        {
+            Conteo = 0;
+        }
+
+        public void Contar()
+        {
+            Conteo++;
+        }
+        public void Reiniciar()
+        {
+            Conteo = 0;
+        }
+}
+
+        }
