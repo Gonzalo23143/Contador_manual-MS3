3fd3bcfd29a0ad0dc03ea88ba5872bcecfbfd907
Gonzalo Cutz Gasca <24470027@campeche.tecnm.mx|git config user.email 24470027@campeche.tecnm.mx>
2025-10-14 18:19:17 -0600

    Agregacion de la programacion orientada a objetos en forma de delegacion con clases

--- Conador_Manual2/MainPage.xaml.cs
+++ Conador_Manual2/MainPage.xaml.cs
@@ -1,41 +1,43 @@
-﻿namespace Conador_Manual2
+﻿using Conador_Manual2.NewFolder;
+
+namespace Conador_Manual2
 {
     public partial class MainPage : ContentPage
     {
         // Conteo lleva el conteo en la aplicacion
-        private int Conteo = 0;
+        private Class1 contador;
         public MainPage()
         {
             InitializeComponent();
-            Conteo = 0;
-            ConteoLabel.Text = Conteo.ToString();
+            contador = new Class1();
+            ConteoLabel.Text = contador.Conteo.ToString();
+
         }
 
         private void Button_Clicked(object sender, EventArgs e)
         {
-            Conteo ++;
-            ConteoLabel.Text = Conteo.ToString();
+            contador.Contar();
+            ConteoLabel.Text = contador.Conteo.ToString();
+            
 
         }
 
         private void Button_Clicked_1(object sender, EventArgs e)
         {
-            Conteo = 0;
-            ConteoLabel.Text = Conteo.ToString();
 
+            contador.Reiniciar();
+            ConteoLabel.Text = contador.Conteo.ToString();
 
         }
 
         private void Button_Clicked_2(object sender, EventArgs e)
         {
-            Conteo = Conteo+5;
-            ConteoLabel.Text = Conteo.ToString();
+          
         }
 
         private void Button_Clicked_3(object sender, EventArgs e)
         {
-            Conteo = Conteo - 1000;
-            ConteoLabel.Text = Conteo.ToString();
+           
         }
     }
 }
